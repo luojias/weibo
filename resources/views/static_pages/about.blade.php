481259ee6d8bf619599e0b2c3f1ae2fe8581205c
LuoJia <176064056@qq.com>
2019-12-21 06:40:45 +0000

    初始化样式

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
 @section('title', '关于')
 @section('content')
-  <h1>关于页</h1>
+  <h1>他的关于页</h1>
 @stop
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
 @section('title', '帮助')
 @section('content')
-  <h1>帮助页</h1>
+  <h1>你的帮助页</h1>
 @stop
